6d20005fb04d097683d35e0e57b5711b718c8167
Joshua Miller <joshua.miller@fitsnips.net>
2024-12-15 15:48:44 -0600

    Add helloword-args add full os.Args to output

--- projects/helloworld-args/main.go
+++ projects/helloworld-args/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	args := os.Args
 
 	fmt.Printf("Hello world\nos.Args: %v\nArguments: %v\n", args, args[1:])
+
+	for i := 0; i < len(args); i++ {
+		if strings.HasPrefix(args[i],"--")
+			fmt.Println("Args: %v\n, Value: %v", args[i], args[i++])
+	}
 }
