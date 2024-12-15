3d6a6597fb406a669151652ca21799f8a680c127
Joshua Miller <joshua.miller@fitsnips.net>
2024-12-15 15:56:02 -0600

    Add helloword-args add arg count check

--- projects/helloworld-args/main.go
+++ projects/helloworld-args/main.go
@@ -3,16 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
 	args := os.Args
 
-	fmt.Printf("Hello world\nos.Args: %v\nArguments: %v\n", args, args[1:])
-
-	for i := 0; i < len(args); i++ {
-		if strings.HasPrefix(args[i],"--")
-			fmt.Println("Args: %v\n, Value: %v", args[i], args[i++])
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./hello-world <argument>")
+		os.Exit(1)
 	}
+
+	fmt.Printf("Hello world\nos.Args: %v\nArguments: %v\n", args, args[1:])
 }
